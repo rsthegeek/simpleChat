cd36778fe02c3052eedd258e59da55ee7b1e7c7c
MohammadR Fekri <rasoolsystem@me.com>
2020-08-17 03:29:48 +0430

    final changes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@
 
 Auth::routes();
 
+Route::redirect('/home', '/');
 Route::get('/', 'ChatsController@index');
 Route::get('messages', 'ChatsController@fetchMessages');
 Route::post('messages', 'ChatsController@sendMessage');
